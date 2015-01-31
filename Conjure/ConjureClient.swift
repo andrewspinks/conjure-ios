18d253db433bb26006364456e0951b06ca8a230d
Andrew Spinks <andrewspinks@gmail.com>
2015-01-31 11:45:40 +1100

    Install certificate to resolve signing problems of dependencies on travisci.

--- Conjure/ConjureClient.swift
+++ Conjure/ConjureClient.swift
@@ -1,9 +1,3 @@
-//
-//  ConjureClient.swift
-//  Conjure
-//
-//  Created by Andrew Spinks on 30/01/2015.
-//  Copyright (c) 2015 Andrew Spinks. All rights reserved.
-//
 
-import Foundation
+import Alamofire
+
